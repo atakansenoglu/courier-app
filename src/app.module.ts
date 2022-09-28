ae9eeeddb8b8c5b4be680af1f49ee23505b29071
Atakan <atakan.senoglu@viravira.co>
2022-09-28 13:38:20 +0300

    Credenitials fixed.

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { CourierModule } from './couriers/courier.module';
 @Module({
   imports: [
     MongooseModule.forRoot(
-      `mongodb+srv://atakansenoglu:atakansenoglu@cluster0.rag4vwa.mongodb.net/?retryWrites=true&w=majority`,
+      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/?retryWrites=true&w=majority`,
     ),
     CourierModule,
   ],
